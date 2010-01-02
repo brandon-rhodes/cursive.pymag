15c3a81bf989980469a2c3a8d5f83640c3c5e79c
Doug Hellmann <doug.hellmann@gmail.com>
2010-01-02 15:36:49 -0500

    add ceres parser and stub in command for rst conversion

---
+++ torst.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
+#
+"""Convert Ceres documents to reStructuredText.
+"""
+
+# Import system modules
+
+
+# Import third-party modules
+
+
+# Import local modules
+from cursive.pymag import ceres
+
+def command():
+    """Convert a Ceres document to reStructuredText
+    """
+    return
+
