8a64954db2371d4105b7cf5fed23381b6c5ebbe4
Doug Hellmann <doug.hellmann@gmail.com>
2010-04-23 06:53:32 -0400

    don't need pkgutil with pyron

--- __init__.py
+++ __init__.py
@@ -6,6 +6,3 @@ __testrunner__ = 'nose'
 __requires__ = [ 'cursive.tools', 'docutils' ]
 __author__ = 'Brandon Craig Rhodes <brandon@rhodesmill.org>'
 __url__ = 'http://bitbucket.org/brandon/cursivepymag/'
-
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
